1a99d090fdcc20d12fa8a489d668ec8d880a1dbe
Nikos Kon <nikoskontothanasis@users.noreply.github.com>
2017-12-03 14:45:48 +0200

    add all

--- ptyxiaki/ViewController.swift
+++ ptyxiaki/ViewController.swift
@@ -6,20 +6,79 @@
 //  Copyright © 2017 Nikos Kontothanasis. All rights reserved.
 //
 
+import MapKit
 import UIKit
+import FirebaseDatabase
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    var mapHasCenteredOnce = false
+    var geoFire: GeoFire!
+    var geoFireRef: DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
+        mapView.userTrackingMode = MKUserTrackingMode.follow
+        
+        geoFireRef = Database.database().reference()
+        geoFire = GeoFire(firebaseRef: geoFireRef)
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        locationAuthStatus()
     }
-
-
+    
+    func locationAuthStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        }
+        
+    }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 200, 200)
+        
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        if let loc = userLocation.location
+        {
+            if !mapHasCenteredOnce {
+                centerMapOnLocation(location: loc)
+                mapHasCenteredOnce=true
+            }
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        var annotationView: MKAnnotationView?
+        if annotation.isKind(of: MKUserLocation.self) {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
+            annotationView?.image = UIImage(named: "")
+        }
+        return annotationView
+    }
+    
+    func createSighting(forLocation location: CLLocation, withViews viewId: Int) {
+        geoFire.setLocation(location, forKey: "\(viewId)")
+    }
+    @IBAction func spotRandom(_ sender: Any) {
+    }
+    
 }
-
---
+++ ptyxiaki/ptyxiaki-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  ptyxiaki-Bridging-Header.h
+//  ptyxiaki
+//
+//  Created by Nikos Kontothanasis on 01/12/2017.
+//  Copyright © 2017 Nikos Kontothanasis. All rights reserved.
+//
+
+#import "GeoFire.h"
